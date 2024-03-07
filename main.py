b4601fc3965bd010d2350b7294839943b397eabd
ALUNO <aluno@ifsp.edu.br>
2024-03-06 21:06:42 -0300

    alteração categoria e produtos

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from sqlalchemy import (Column, Uuid, String, DateTime,
 from sqlalchemy import create_engine, select
 from sqlalchemy.orm import DeclarativeBase, relationship, Session
 
-motor = create_engine("sqlite+pysqlite:///banco_de_dados.sqlite", echo=True)
+motor = create_engine("sqlite+pysqlite:///banco_de_dados.sqlite", echo=False)
 
 
 class Base(DeclarativeBase):
@@ -29,6 +29,7 @@ class Categoria(Base):
                 default=uuid.uuid4)
     nome = Column(String(256),
                   nullable=False)
+    # Propriedade de navegação
 
     lista_de_produtos = relationship("Produto", back_populates="categoria",
                                      cascade='all, delete-orphan', lazy='selectin')
@@ -43,39 +44,66 @@ class Produto(Base, DatasMixin):
     ativo = Column(Boolean, default=True)
     Categoria_id = Column(Uuid(as_uuid=True), ForeignKey("tbl_categorias.id"))
 
+#Propriedade de navegação
     categoria = relationship("Categoria", back_populates="lista_de_produtos")
 
+def seed_database():
+    with Session(motor) as sessao:
+        if sessao.execute(select(Categoria).limit(1)).scalar_one_or_none():
+            return
+        from seed import seed_data
+        for categoria in seed_data:
+            cat = Categoria()
+            print(f"Semeado a categoria {categoria['categoria']}...")
+            cat.nome = categoria['categoria']
+            for produto in categoria['produtos']:
+                p= Produto()
+                p.nome = produto['nome']
+                p.preco = produto['preco']
+                p.estoque = 0
+                p.ativo = True
+                p.categoria = cat
+                sessao.add(p)
+            sessao.commit ()
 
-cat = Categoria()
-cat.nome = "Bebidas"
+def incluir_categoria():
+    print("Incluindo categoria")
+    nome = input("Qual o nome da categoria que você quer adicionar? ")
+    with Session(motor) as sessao:
+        categoria = Categoria()
+        categoria.nome = nome
+        sessao.add(categoria)
+        sessao.commit()
+    print(f"Categoria {nome} adicionada.")
 
-prod = Produto()
-prod.nome = "Coca cola zero, 2L"
-prod.ativo = True
-prod.preco = 9.50
-prod.estoque = 100
-prod.categoria = cat
+def listar_categorias():
+    print("Categorias cadastradas")
+    print(f"Nome                                      # Produtos")
+    print(f"----------------------------------------- ----------")
+    stmt = select(Categoria)
+    stmt = stmt.order_by("nome")
+    with Session(motor) as sessao:
+        rset = sessao.execute(stmt).scalars()
+        for categoria in rset:
+            print(f"{categoria.nome:40s}  {len(categoria.lista_de_produtos):10d}")
+            # for produto in categoria.lista_de_produtos:
+            #     print(f"   {produto.nome}")
+    print(f"----------------------------------------- ----------")
 
-with Session(motor) as sessao:
-    sessao.add(prod)
-    sessao.commit()
+if __name__ == "__main__":
+    seed_database()
+    while True:
+        print("Menu de opcoes")
+        print("1. Incluir categoria")
+        print("2. Listar categorias")
+        print("0. Sair")
+        opcao = int(input("Qual opcao? "))
+        if opcao == 1:
+            incluir_categoria()
+        elif opcao == 2:
+            listar_categorias()
+        elif opcao == 0:
+            exit(0)
+        else:
+            print("Opcao invalida...")
 
-
-cat = Categoria()
-cat.nome = "Salgadinho"
-
-prod = Produto()
-prod.nome = "Doritos, 600gr"
-prod.ativo = True
-prod.preco = 12.94
-prod.estoque = 0
-prod.categoria = cat
-
-with Session(motor) as sessao:
-    sessao.add(prod)
-    sessao.commit()
-
-with Session(motor) as sessao:
-    categorias = sessao.execute(select(Categoria).where(Categoria.nome == "Bebidas")).scalars()
-    for categoria in categorias:
-        print(f"A categoria{categoria.nome} tem {len(categoria.lista_de_produtos)} produtos")
